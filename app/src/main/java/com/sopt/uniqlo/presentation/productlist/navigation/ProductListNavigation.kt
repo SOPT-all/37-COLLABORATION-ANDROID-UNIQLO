294e0cb2f207da9211580741b55346c24a4ed8c6
seungjae708 <choesj365@gmail.com>
2025-11-20 21:37:54 +0900

    feat/#10 상품 목록 네비게이션 구현

---
+++ app/src/main/java/com/sopt/uniqlo/presentation/productlist/navigation/ProductListNavigation.kt
@@ -0,0 +1,30 @@
+package com.sopt.uniqlo.presentation.productlist.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.sopt.uniqlo.core.navigation.Route
+import com.sopt.uniqlo.presentation.productlist.ProductListRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToProductList(
+    navOptions: NavOptions? = null
+) {
+    navigate(ProductList, navOptions)
+}
+
+fun NavGraphBuilder.productListGraph(
+    paddingValues: PaddingValues,
+    navigateToDetail: () -> Unit
+) {
+    composable<ProductList> {
+        ProductListRoute(
+            paddingValues = paddingValues
+        )
+    }
+}
+
+@Serializable
+data object ProductList : Route
