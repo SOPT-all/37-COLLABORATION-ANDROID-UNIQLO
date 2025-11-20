02ede90fe97183787b57f91fb1edc7cedcf0eda4
seungjae708 <choesj365@gmail.com>
2025-11-20 23:26:57 +0900

    feat/#10 toUiModel 추가

--- app/src/main/java/com/sopt/uniqlo/presentation/productlist/model/ProductUiModel.kt
+++ app/src/main/java/com/sopt/uniqlo/presentation/productlist/model/ProductUiModel.kt
@@ -1,6 +1,9 @@
 package com.sopt.uniqlo.presentation.productlist.model
 
 import androidx.compose.runtime.Immutable
+import com.sopt.uniqlo.domain.productlist.entity.ProductEntity
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
 
 @Immutable
 data class ProductUiModel(
@@ -16,3 +19,21 @@ data class ProductUiModel(
     val reviewCount: Int,
     val isFavorite: Boolean = false
 )
+
+fun List<ProductEntity>.toUiModel(favoriteMap : Map<Long, Boolean>) : PersistentList<ProductUiModel> {
+    return map { entity ->
+        ProductUiModel(
+            id = entity.id,
+            imageUrl = entity.imageUrl,
+            colorHexCodes = entity.colorHexCodes,
+            genderAndSizeRange = entity.genderAndSizeRange,
+            name = entity.name,
+            originalPrice = entity.originalPrice,
+            salePrice = entity.salePrice,
+            productTag = entity.productTag,
+            starAverage = entity.starAverage,
+            reviewCount = entity.reviewCount,
+            isFavorite = favoriteMap [entity.id] ?: false
+        )
+    }.toPersistentList()
+ }
