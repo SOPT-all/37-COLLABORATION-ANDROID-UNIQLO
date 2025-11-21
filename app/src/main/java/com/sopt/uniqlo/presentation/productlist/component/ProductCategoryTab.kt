ad90a1a0f13efb8751cd3dcf72f7ebe4fbaef8cf
seungjae708 <choesj365@gmail.com>
2025-11-21 16:55:50 +0900

    refactor/#10 글씨 스타일 변경

--- app/src/main/java/com/sopt/uniqlo/presentation/productlist/component/ProductCategoryTab.kt
+++ app/src/main/java/com/sopt/uniqlo/presentation/productlist/component/ProductCategoryTab.kt
@@ -45,7 +45,11 @@ fun ProductCategoryTab(
                 text = {
                     Text(
                         text = title,
-                        style = UniqloTheme.typography.caption.m_12,
+                        style = if (index == selectedTabIndex) {
+                            UniqloTheme.typography.reddit.caption_sb_12
+                        } else {
+                            UniqloTheme.typography.reddit.caption_m_12
+                        },
                         color = if (index == selectedTabIndex) {
                             UniqloTheme.colors.black
                         } else {
