bb2a80315f187a807350f6df68d9a5ab3a6a1cad
SHC <hcshin0717@naver.com>
2025-11-22 15:31:16 +0900

    add/#8 - 제품 상세 설명 Model 생성

---
+++ app/src/main/java/com/sopt/uniqlo/presentation/detailpage/model/DetailDescriptionModel.kt
@@ -0,0 +1,9 @@
+package com.sopt.uniqlo.presentation.detailpage.model
+
+data class DetailDescriptionModel(
+    val detailPageUrl: String? = null,
+    val detailText: String,
+    val descriptionText: List<String>,
+    val featureDetailText: List<String>,
+    val sizeDetailText: List<String>,
+)
