880ebf2bca4a7f3aa781bc562e179c28cb674883
SHC <hcshin0717@naver.com>
2025-11-22 13:22:22 +0900

    feat/#8 - ReviewStarPoint 생성

---
+++ app/src/main/java/com/sopt/uniqlo/presentation/detailpage/component/ReviewStarPoint.kt
@@ -0,0 +1,40 @@
+package com.sopt.uniqlo.presentation.detailpage.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.uniqlo.R
+import com.sopt.uniqlo.core.designsystem.theme.UniqloTheme
+
+@Composable
+fun ReviewStarPoint(
+    modifier: Modifier = Modifier,
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        repeat(5) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_star_filled),
+                contentDescription = null,
+                tint = UniqloTheme.colors.black,
+                modifier = Modifier
+                    .size(18.dp)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun ReviewStarPointPreview() {
+    ReviewStarPoint()
+}
