6f193f01c574c627e6f8b9964742662b35713b09
seungjae708 <choesj365@gmail.com>
2025-11-25 02:27:04 +0900

    refactor/#14 서버 구조에 따라 BaseResponse 변경

--- app/src/main/java/com/sopt/uniqlo/core/network/model/BaseResponse.kt
+++ app/src/main/java/com/sopt/uniqlo/core/network/model/BaseResponse.kt
@@ -3,13 +3,12 @@ package com.sopt.uniqlo.core.network.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// Todo : 추루 서버 구조에 따라 변경 예정
 @Serializable
 data class BaseResponse<T>(
     @SerialName("code")
-    val code: String,
-    @SerialName("message")
-    val message: String,
+    val code: Long,
+    @SerialName("msg")
+    val msg: String,
     @SerialName("data")
     val data: T? = null
 )
