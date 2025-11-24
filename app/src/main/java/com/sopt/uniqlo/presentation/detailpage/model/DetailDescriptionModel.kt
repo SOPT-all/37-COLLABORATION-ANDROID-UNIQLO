35f894c2d6cdf74b2a6d2b76c06d8b7c5787024b
SHC <hcshin0717@naver.com>
2025-11-24 18:52:01 +0900

    mod/#8 - DetailDescription 모델 nullable 수정

--- app/src/main/java/com/sopt/uniqlo/presentation/detailpage/model/DetailDescriptionModel.kt
+++ app/src/main/java/com/sopt/uniqlo/presentation/detailpage/model/DetailDescriptionModel.kt
@@ -1,7 +1,7 @@
 package com.sopt.uniqlo.presentation.detailpage.model
 
 data class DetailDescriptionModel(
-    val detailPageUrl: String? = null,
+    val detailPageUrl: String = "",
     val detailText: String,
     val descriptionText: List<String>,
     val featureDetailText: List<String>,
