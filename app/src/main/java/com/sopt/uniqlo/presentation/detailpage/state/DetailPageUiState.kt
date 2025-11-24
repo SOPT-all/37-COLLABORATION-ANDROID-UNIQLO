01464aa5ea1ecedb23252cc4e68205102e6c79a2
SHC <hcshin0717@naver.com>
2025-11-24 18:48:09 +0900

    mod/#8 - DetailPage UiState 값 수정

--- app/src/main/java/com/sopt/uniqlo/presentation/detailpage/state/DetailPageUiState.kt
+++ app/src/main/java/com/sopt/uniqlo/presentation/detailpage/state/DetailPageUiState.kt
@@ -1,10 +1,25 @@
 package com.sopt.uniqlo.presentation.detailpage.state
 
+import com.sopt.uniqlo.presentation.detailpage.TabState
 import com.sopt.uniqlo.presentation.detailpage.model.DetailDescriptionModel
+import com.sopt.uniqlo.presentation.detailpage.model.ReviewModel
 import com.sopt.uniqlo.presentation.detailpage.model.SizeInformationItemModel
+import com.sopt.uniqlo.presentation.detailpage.model.StyleHintModel
 
 data class DetailPageUiState (
-    val detailDescriptionList: DetailDescriptionModel? = null,
+    val tabState: TabState = TabState.DETAIL,
+    val detailDescription: DetailDescriptionModel = DetailDescriptionModel(
+        detailPageUrl = "",
+        detailText = "",
+        descriptionText = emptyList(),
+        featureDetailText = emptyList(),
+        sizeDetailText = emptyList()
+    ),
     val sizeInformationList: List<SizeInformationItemModel> = emptyList(),
+    val styleHintList: List<StyleHintModel> = emptyList(),
+    val reviewList: List<ReviewModel> = emptyList(),
+    val reviewStarPointAverage: Float = 4.8f,
+    val reviewFitAverage: Int = 5,
+    val isWished: Boolean = false,
     val isLoading: Boolean = false,
 )
