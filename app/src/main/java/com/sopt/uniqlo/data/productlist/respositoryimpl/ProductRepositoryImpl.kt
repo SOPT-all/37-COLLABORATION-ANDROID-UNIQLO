f2c2aabe86c1e26b0c257420ab0eb4681e9e7908
seungjae708 <choesj365@gmail.com>
2025-11-25 01:55:57 +0900

    feat/#14 ProductRepositoryImpl 구현

---
+++ app/src/main/java/com/sopt/uniqlo/data/productlist/respositoryimpl/ProductRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.sopt.uniqlo.data.productlist.respositoryimpl
+
+import com.sopt.uniqlo.core.util.suspendRunCatching
+import com.sopt.uniqlo.data.productlist.datasource.ProductDataSource
+import com.sopt.uniqlo.domain.productlist.entity.ProductEntity
+import com.sopt.uniqlo.domain.productlist.repository.ProductRepository
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+    private val productDataSource: ProductDataSource,
+) : ProductRepository {
+    override suspend fun getProducts(): Result<List<ProductEntity>> = suspendRunCatching {
+        productDataSource.getProducts().map { it.toProductEntity() }
+    }
+}
