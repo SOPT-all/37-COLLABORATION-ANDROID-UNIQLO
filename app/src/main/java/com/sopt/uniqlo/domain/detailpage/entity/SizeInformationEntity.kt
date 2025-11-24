8b3bc9bdfcdb72a3529b96891b0bc98bf04b8418
SHC <hcshin0717@naver.com>
2025-11-24 19:29:30 +0900

    del/#8 - sizeInformation 관련 domain 파일 제거

--- app/src/main/java/com/sopt/uniqlo/domain/detailpage/entity/SizeInformationEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.sopt.uniqlo.domain.detailpage.entity
-data class SizeInformationEntity(
-    val imgResource: Int,
-    val title: String,
-    val description: String,
-    val imgDescription: String? = null,
-)
