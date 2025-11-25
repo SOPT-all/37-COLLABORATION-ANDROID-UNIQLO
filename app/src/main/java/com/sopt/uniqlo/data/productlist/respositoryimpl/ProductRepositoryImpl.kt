6d8b45350d522603ad89eb38eddcca29adc10a57
seungjae708 <choesj365@gmail.com>
2025-11-25 23:20:26 +0900

    feat/#14 ProductRepository/Impl 구현

--- app/src/main/java/com/sopt/uniqlo/data/productlist/respositoryimpl/ProductRepositoryImpl.kt
+++ app/src/main/java/com/sopt/uniqlo/data/productlist/respositoryimpl/ProductRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.sopt.uniqlo.data.productlist.respositoryimpl
 
 import com.sopt.uniqlo.core.util.suspendRunCatching
 import com.sopt.uniqlo.data.productlist.datasource.ProductDataSource
+import com.sopt.uniqlo.domain.productdetail.entity.ProductDetailEntity
 import com.sopt.uniqlo.domain.productlist.entity.ProductEntity
 import com.sopt.uniqlo.domain.productlist.repository.ProductRepository
 import javax.inject.Inject
@@ -12,4 +13,9 @@ class ProductRepositoryImpl @Inject constructor(
     override suspend fun getProducts(): Result<List<ProductEntity>> = suspendRunCatching {
         productDataSource.getProducts().map { it.toProductEntity() }
     }
+
+    override suspend fun getProductDetail(productId: Long): Result<ProductDetailEntity> =
+        suspendRunCatching {
+            productDataSource.getProductDetail(productId).toProductDetailEntity()
+        }
 }
--- app/src/main/java/com/sopt/uniqlo/domain/productlist/repository/ProductRepository.kt
+++ app/src/main/java/com/sopt/uniqlo/domain/productlist/repository/ProductRepository.kt
@@ -1,7 +1,10 @@
 package com.sopt.uniqlo.domain.productlist.repository
 
+import com.sopt.uniqlo.domain.productdetail.entity.ProductDetailEntity
 import com.sopt.uniqlo.domain.productlist.entity.ProductEntity
 
 interface ProductRepository {
     suspend fun getProducts(): Result<List<ProductEntity>>
+
+    suspend fun getProductDetail(productId: Long): Result<ProductDetailEntity>
 }
