ffd67d0710dece232e33354adc752286185bee90
seungjae708 <choesj365@gmail.com>
2025-11-25 23:21:22 +0900

    feat/#14 GetProductDetailUseCase 구현

---
+++ app/src/main/java/com/sopt/uniqlo/domain/productdetail/usecase/GetProductDetailUseCase.kt
@@ -0,0 +1,13 @@
+package com.sopt.uniqlo.domain.productdetail.usecase
+
+import com.sopt.uniqlo.domain.productdetail.entity.ProductDetailEntity
+import com.sopt.uniqlo.domain.productlist.repository.ProductRepository
+import javax.inject.Inject
+
+class GetProductDetailUseCase @Inject constructor(
+    private val productRepository: ProductRepository
+) {
+    suspend operator fun invoke(productId: Long): Result<ProductDetailEntity> {
+        return productRepository.getProductDetail(productId)
+    }
+}
