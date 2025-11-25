ee56b76a2ab931ab16a0c512851aa3cbbe041781
seungjae708 <choesj365@gmail.com>
2025-11-25 23:19:19 +0900

    feat/#14 ProductService 정의 추가

--- app/src/main/java/com/sopt/uniqlo/data/productlist/service/ProductService.kt
+++ app/src/main/java/com/sopt/uniqlo/data/productlist/service/ProductService.kt
@@ -1,10 +1,17 @@
 package com.sopt.uniqlo.data.productlist.service
 
 import com.sopt.uniqlo.core.network.model.BaseResponse
+import com.sopt.uniqlo.data.productlist.dto.ProductDetailResponseDto
 import com.sopt.uniqlo.data.productlist.dto.ProductResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductService {
     @GET("products")
     suspend fun getProducts(): BaseResponse<List<ProductResponseDto>>
+
+    @GET("products/{productId}")
+    suspend fun getProductDetail(
+        @Path("productId") productId: Long
+    ): BaseResponse<ProductDetailResponseDto>
 }
