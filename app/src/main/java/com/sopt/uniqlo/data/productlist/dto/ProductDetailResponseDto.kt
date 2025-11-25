1d505c515ab1e4ace19b28579e2257a4379f5a10
seungjae708 <choesj365@gmail.com>
2025-11-25 23:18:26 +0900

    feat/#14 ProductDetailResponseDto 정의 및 Entity 변환 함수 정의

---
+++ app/src/main/java/com/sopt/uniqlo/data/productlist/dto/ProductDetailResponseDto.kt
@@ -0,0 +1,39 @@
+package com.sopt.uniqlo.data.productlist.dto
+
+import com.sopt.uniqlo.domain.productdetail.entity.ProductDetailEntity
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductDetailResponseDto(
+    @SerialName("productId")
+    val productId: Long,
+    @SerialName("productImageUrl")
+    val productImageUrl: List<String>,
+    @SerialName("colorCode")
+    val colorCode: List<String>,
+    @SerialName("color")
+    val colorMap: Map<String, String>,
+    @SerialName("name")
+    val name: String,
+    @SerialName("originPrice")
+    val originPrice: String,
+    @SerialName("salePrice")
+    val salePrice: String? = null,
+    @SerialName("starAverage")
+    val starAverage: Float,
+    @SerialName("reviewCount")
+    val reviewCount: Int
+) {
+    fun toProductDetailEntity() = ProductDetailEntity(
+        id = productId,
+        imageUrl = productImageUrl,
+        colorHexCodes = colorCode,
+        colorMap = colorMap,
+        name = name,
+        originPrice = originPrice.removeSuffix("원"),
+        salePrice = salePrice?.removeSuffix("원"),
+        starAverage = starAverage,
+        reviewCount = reviewCount
+    )
+}
