9c944c62cf5e6bce06d4893998ec48d1facb042a
SHC <hcshin0717@naver.com>
2025-11-26 11:26:18 +0900

    mod/#16 - BaseResponse 변경

--- app/src/main/java/com/sopt/uniqlo/core/network/model/BaseResponse.kt
+++ app/src/main/java/com/sopt/uniqlo/core/network/model/BaseResponse.kt
@@ -7,9 +7,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class BaseResponse<T>(
     @SerialName("code")
-    val code: String,
-    @SerialName("message")
-    val message: String,
+    val code: Long,
+    @SerialName("msg")
+    val msg: String,
     @SerialName("data")
     val data: T? = null
 )
