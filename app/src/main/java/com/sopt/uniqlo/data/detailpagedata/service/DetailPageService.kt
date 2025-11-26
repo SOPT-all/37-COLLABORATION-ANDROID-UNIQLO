3b68d0495db4e3ae644276c39c73a952b6f3c874
SHC <hcshin0717@naver.com>
2025-11-26 12:07:47 +0900

    feat/#16 - DetailPage 서비스 추가

---
+++ app/src/main/java/com/sopt/uniqlo/data/detailpagedata/service/DetailPageService.kt
@@ -0,0 +1,21 @@
+package com.sopt.uniqlo.data.detailpage.service
+
+import com.sopt.uniqlo.core.network.model.BaseResponse
+import com.sopt.uniqlo.data.detailpage.dto.ProductDetailResponseDto
+import com.sopt.uniqlo.data.detailpage.dto.StyleHintListResponseDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DetailPageService {
+
+    @GET("products/{productId}/details")
+    suspend fun getProductDetail(
+        @Path("productId") productId: Int
+    ): Response<BaseResponse<ProductDetailResponseDto>>
+
+    @GET("products/{productId}/hints")
+    suspend fun getStyleHintList(
+        @Path("productId") productId: Int
+    ): Response<BaseResponse<StyleHintListResponseDto>>
+}
