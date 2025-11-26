54c1df792416053208f1f896589999193ee83a9a
SHC <hcshin0717@naver.com>
2025-11-26 12:09:03 +0900

    feat/#16 - Model 관련 toUiModel 추가

--- app/src/main/java/com/sopt/uniqlo/presentation/detailpage/model/DetailDescriptionModel.kt
+++ app/src/main/java/com/sopt/uniqlo/presentation/detailpage/model/DetailDescriptionModel.kt
@@ -1,5 +1,7 @@
 package com.sopt.uniqlo.presentation.detailpage.model
 
+import com.sopt.uniqlo.domain.detailpage.entity.ProductDetailEntity
+
 data class DetailDescriptionModel(
     val detailPageUrl: List<String>,
     val detailText: String,
@@ -7,3 +9,13 @@ data class DetailDescriptionModel(
     val featureDetailText: List<String>,
     val sizeDetailText: List<String>,
 )
+
+fun ProductDetailEntity.toUiModel() : DetailDescriptionModel {
+    return DetailDescriptionModel(
+        detailPageUrl = detailImageUrl,
+        detailText = detailText,
+        descriptionText = descriptionText,
+        featureDetailText = featureDetailText,
+        sizeDetailText = sizeDetailText
+    )
+}
